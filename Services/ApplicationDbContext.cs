a8afe027d1422b5b10958c68cc60ef9e79f27ba4
Knarik <harutyunyanqnarik2@gmail.com>
2024-09-12 17:15:23 +0400

    first commit

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VowelsProject.Services
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
